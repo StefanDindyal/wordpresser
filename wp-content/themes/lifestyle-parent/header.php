8a427d7a7d72386b8a5fb7811c6f6d59a7062711
Stefan Dindyal <stefan@rgnrtr.com>
2013-10-22 22:29:26 -0400

    update

--- wp-content/themes/lifestyle-parent/header.php
+++ wp-content/themes/lifestyle-parent/header.php
@@ -22,6 +22,18 @@
             wp_head();
             ?>
             <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+            
+            <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45013704-1', 'girlthatsmysong.com');
+  ga('send', 'pageview');
+
+  			</script>
+
         </head>
         <body>
         <?php /*<div class="head_bar">
