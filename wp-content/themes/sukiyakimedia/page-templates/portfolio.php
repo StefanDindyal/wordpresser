d848d544ad90f4ebea2352e0702bd53d73fba912
StefanDindyal <stefan@rgnrtr.com>
2015-05-18 14:58:17 -0400

    update

---
+++ wp-content/themes/sukiyakimedia/page-templates/portfolio.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Template Name: Portfolio
+*/
+get_header(); ?>
+<div id="main-content" class="main-content port">
+	<section id="works" class="b">
+		<h2 class="section-title">Works</h2>
+		<ul>
+			<li>
+				<a href="http://disneyvisa.myplaydirect.com/" target="_blank">
+					<span class="hi">
+						<img src="<?php bloginfo('template_directory'); ?>/images/port/visa.png" alt="" border="0">
+					</span>
+					<span class="title">The Legacy Collection</span>
+				</a>
+			</li>
+			<li>
+				<a href="http://www.disneymusicemporium.com/" target="_blank">
+					<span class="hi">
+						<img src="<?php bloginfo('template_directory'); ?>/images/port/disney.png" alt="" border="0">
+					</span>
+					<span class="title">Disney Music Emporium</span>
+				</a>
+			</li>
+			<li>
+				<a href="http://www.ollymurs.com/home/" target="_blank">
+					<span class="hi">
+						<img src="<?php bloginfo('template_directory'); ?>/images/port/olly.png" alt="" border="0">
+					</span>
+					<span class="title">Olly Murs</span>
+				</a>
+			</li>
+			<li>
+				<a href="http://www.palomafaith.com/en-us/" target="_blank">
+					<span class="hi">
+						<img src="<?php bloginfo('template_directory'); ?>/images/port/paloma.png" alt="" border="0">
+					</span>
+					<span class="title">Paloma Faith</span>
+				</a>
+			</li>
+			<li>
+				<a href="http://www.sonymusiclatin.com/" target="_blank">
+					<span class="hi">
+						<img src="<?php bloginfo('template_directory'); ?>/images/port/latin.png" alt="" border="0">
+					</span>
+					<span class="title">Sony Music Latin</span>
+				</a>
+			</li>
+			<li>
+				<a href="http://www.watchmedoubletap.com/" target="_blank">
+					<span class="hi">
+						<img src="<?php bloginfo('template_directory'); ?>/images/port/tap.png" alt="" border="0">
+					</span>
+					<span class="title">Watch Me Double Tap</span>
+				</a>
+			</li>			
+		</ul>
+		<div class="clear"></div>
+	</section>
+</div><!-- #main-content -->
+<?php
+get_footer();
---
+++ wp-content/themes/sukiyakimedia/page-templates/pre.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: Pre
+*/
+get_header(); ?>
+<div id="main-content" class="main-content pre">
+	<section id="about" class="b">
+		<h2 class="section-title">About Us</h2>
+		<div id="us">
+			Full site coming soon!<br>
+			Check us out on Facebook: <a href="https://www.facebook.com/sukiyakimedia">Sukiyaki Media</a>
+		</div>
+		<ul id="members">
+			<li class="caleb">
+				<div class="avatar"></div>
+				<span>Caleb</span>
+			</li>
+			<li class="mark">
+				<div class="avatar"></div>
+				<span>Mark</span>
+			</li>
+			<li class="stefan">
+				<div class="avatar"></div>
+				<span>Stefan</span>
+			</li>
+		</ul>
+		<div class="clear"></div>
+	</section>
+</div>
+<?php
+get_footer();
