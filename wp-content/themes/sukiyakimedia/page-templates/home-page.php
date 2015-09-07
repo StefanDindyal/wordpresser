4d52bca7906e99cc00dd5dedabfa29d4bd86bc2b
StefanDindyal <stefan@rgnrtr.com>
2015-09-07 02:57:59 -0400

    update

--- wp-content/themes/sukiyakimedia/page-templates/home-page.php
+++ wp-content/themes/sukiyakimedia/page-templates/home-page.php
@@ -10,7 +10,7 @@ get_header(); ?>
 		get_template_part( 'featured-content' );
 	}
 ?>
-	<? /* <section id="news" class="b">
+	<?php /* <section id="news" class="b">
 		<h2 class="section-title">News</h2>
 		<?php
 			if ( have_posts() ) :
@@ -22,6 +22,68 @@ get_header(); ?>
 			endif;
 		?>
 	</section> */ ?>
+	<section id="works" class="b">
+		<h2 class="section-title">Works</h2>
+		<ul class="story">
+			<li class="wod">
+				<div class="large-img"></div>
+				<span>Wings of Destiny</span>				
+				<p>
+					A powerful force once thought to be banished forever is destined to return. The future of civilization rests in the fate of the chosen few, with fates intertwined by prophecy, are destined to bring balance within the dimensions. One such fated person is a young man named Ivran Price.  A daunting task lies ahead for the “Soldier of Destiny”. Gifted with unique abilities and chosen by a magic sword, he is fated to lead his allies to destroy the looming onslaught that is drawing near &hellip;
+				</p>
+				<em>Creator: Caleb</em>
+				<ul class="gallery">
+					<li>
+						<a href="#"></a>
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+				</ul>
+			</li>
+			<li class="ew">
+				<div class="large-img"></div>
+				<span>Eternal Wonderer</span>				
+				<p>
+					A man, by the name of Myros, strikes a terrible deal with power beyond his imagination for a chance to live again; all for the sake of protecting his love ones that he was forced to leave behind. Myros embarks into a journey for answers that will question his morals, push his resolve to the limits, and test what remains of his humanity.
+				</p>
+				<em>Creator: Mark</em>
+				<ul class="gallery">
+					<li>
+						<a href="#"></a>
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+				</ul>
+			</li>
+			<li class="el">
+				<div class="large-img"></div>
+				<span>Project: Death Sentence</span>				
+				<p>
+					“I never asked to be born the way that I was. I never asked to be born with the fate that I was given. I never wanted any of this. I never had a choice.” Eleanor is the story that we have worked on as a team for some time now, over the past few years. Because of my own personal feelings about the development of Eleanor, she has been given a bit of a vacation, not that she doesn’t deserve one. Death Sentence is a variant story of Eleanor that I have decided to take on. A version of the story that exists outside of what was originally concepted but with enough overlap to maintain some of its heart. I hope to use this tale to put my own heart to rest and get back to Eleanor down the line. “There is always a choice, Eleanor. It’s how you live with the consequences of that choice that will define you.”
+				</p>
+				<em>Creator: Stefan</em>
+				<ul class="gallery">
+					<li>
+						<a href="#"></a>
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</section>
 	<section id="about" class="b">
 		<h2 class="section-title">About Us</h2>
 		<div class="ico">
@@ -39,13 +101,13 @@ get_header(); ?>
 				<div class="avatar"></div>
 				<span>Caleb</span>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+					I am an Army Veteran with a passion for creativity involving writing and film making. I'm a writer, non-union actor and am pursuing a career as a film maker. My goal is to share and create my ideas through webcomics, TV and film.
 				</p>
 				<strong>Skills</strong>
 				<ul>
-					<li>Skill</li>
-					<li>Skill</li>
-					<li>Skill</li>
+					<li><span>&raquo;</span> Writing</li>
+					<li><span>&raquo;</span> Screenwriting</li>
+					<li><span>&raquo;</span> Film Production</li>
 				</ul>
 			</li>
 			<li class="mark">
@@ -56,8 +118,8 @@ get_header(); ?>
 				</p>
 				<strong>Skills</strong>
 				<ul>
-					<li>Photography</li>
-					<li>Videography</li>					
+					<li><span>&raquo;</span> Photography</li>
+					<li><span>&raquo;</span> Videography</li>					
 				</ul>
 			</li>
 			<li class="stefan">
@@ -68,72 +130,13 @@ get_header(); ?>
 				</p>
 				<strong>Skills</strong>
 				<ul>
-					<li>Web Development</li>
-					<li>Illustrator</li>					
+					<li><span>&raquo;</span> Web Development</li>
+					<li><span>&raquo;</span> Illustrator</li>					
 				</ul>
 			</li>
 		</ul>
 		<div class="clear"></div>
-	</section>
-	<section id="works" class="b">
-		<h2 class="section-title">Works</h2>
-		<ul class="story">
-			<li class="wod">
-				<div class="large-img"></div>
-				<span>Wings of Destiny</span>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-				</p>
-				<ul class="gallery">
-					<li>
-						<a href="#"></a>
-					</li>
-					<li>
-						<a href="#"></a>
-					</li>
-					<li>
-						<a href="#"></a>
-					</li>
-				</ul>
-			</li>
-			<li class="ew">
-				<div class="large-img"></div>
-				<span>Eternal Wonderer</span>
-				<p>
-					A man, by the name of Myros, strikes a terrible deal with power beyond his imagination for a chance to live again; all for the sake of protecting his love ones that he was forced to leave behind. Myros embarks into a journey for answers that will question his morals, push his resolve to the limits, and test what remains of his humanity.
-				</p>
-				<ul class="gallery">
-					<li>
-						<a href="#"></a>
-					</li>
-					<li>
-						<a href="#"></a>
-					</li>
-					<li>
-						<a href="#"></a>
-					</li>
-				</ul>
-			</li>
-			<li class="el">
-				<div class="large-img"></div>
-				<span>Project: Death Sentence</span>
-				<p>
-					“I never asked to be born the way that I was. I never asked to be born with the fate that I was given. I never wanted any of this. I never had a choice.” Eleanor is the story that we have worked on as a team for some time now, over the past few years. Because of my own personal feelings about the development of Eleanor, she has been given a bit of a vacation, not that she doesn’t deserve one. Death Sentence is a variant story of Eleanor that I have decided to take on. A version of the story that exists outside of what was originally concepted but with enough overlap to maintain some of its heart. I hope to use this tale to put my own heart to rest and get back to Eleanor down the line. “There is always a choice, Eleanor. It’s how you live with the consequences of that choice that will define you.”
-				</p>
-				<ul class="gallery">
-					<li>
-						<a href="#"></a>
-					</li>
-					<li>
-						<a href="#"></a>
-					</li>
-					<li>
-						<a href="#"></a>
-					</li>
-				</ul>
-			</li>
-		</ul>
-	</section>
+	</section>	
 </div><!-- #main-content -->
 <?php
 get_footer();
